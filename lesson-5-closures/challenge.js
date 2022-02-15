13ca2cca3c03c95b8be2c591ffd61a149e7c7e22
valer-sky <89993618+valer-sky@users.noreply.github.com>
2022-02-15 14:55:52 +0300

    lesson-5-closures ready

--- lesson-5-closures/challenge.js
+++ lesson-5-closures/challenge.js
@@ -17,10 +17,10 @@
 var jhon = {
     family: 'Jhon`s',
     bills: [124, 48, 268, 180, 42],
-    arrTips: [],
-    arrBillTips: [],
-    calcTips: function() {
-        for (var i = 0; this.bills.length < 0; i++) {
+    arrTips: [],      // массив для хранения чайевых оставленных джоном
+    arrBillTips: [],   // массив для хранения  оплаченых счетов включая чайевые
+    calcTips: function () {   // метод расчета чайевых
+        for ( var i = 0; i < this.bills.length; i++) {
             if (this.bills[i] < 50) {
                 this.arrTips.push(this.bills[i] * 0.2);
                 } else if (this.bills[i] >= 50 && this.bills[i] <= 200) {
@@ -28,11 +28,57 @@ var jhon = {
                 } else {
                 this.arrTips.push(this.bills[i] * 0.1);
                 }
-            }
-            
             this.arrBillTips.push(this.arrTips[i] + this.bills[i]);
             }
-        
+        }
 }; 
 jhon.calcTips();
-console.log(jhon.arrTips);
+
+// EXTRA AFTER FINISHING: Mark's family also went on a holiday, going to 4 different restaurants. The bills were $77, $375, $110, and $45.
+// Mark likes to tip 20% of the bill when the bill is less than $100, 10% when the bill is between $100 and $300, and 25% if the bill is more than $300 (different than John).
+
+var mark = {
+    family: 'Mark`s',
+    bills: [77, 375, 110, 45],
+    arrTips: [],                         // массив для хранения чайевых оставленных марком
+    arrBillTips: [],                   // массив для хранения  оплаченых счетов включая чайевые
+    calcTips: function () {               // метод расчета чайевых
+        for ( var i = 0; i < this.bills.length; i++) {
+            if (this.bills[i] < 100) {
+                this.arrTips.push(this.bills[i] * 0.2);
+                } else if (this.bills[i] >= 100 && this.bills[i] <= 300) {
+                this.arrTips.push(this.bills[i] * 0.1);
+                } else {
+                this.arrTips.push(this.bills[i] * 0.25);
+                }
+            this.arrBillTips.push(this.arrTips[i] + this.bills[i]);
+            }
+        }
+}; 
+mark.calcTips();
+
+// Create a function (not a method) to calculate the average of a given array of tips. HINT: Loop over the array, and in each iteration store the current sum in a variable (starting from 0). After you have the sum of the array, divide it by the number of elements in it (that's how you calculate the average)
+// Calculate the average tip for each family
+// Log to the console which family paid the highest tips on average
+
+var sum = 0;
+function calcAverage (tips) {    // функция для подсчета средней суммы чайевых каждой семьи
+    for (var i = 0; i < tips.length; i++) {  // цикл для перебора и последующего сумирования всех чайевых
+        sum += tips[i];
+    }
+    return sum / tips.length; //  среднее количество выплаченных чаевых.
+}
+
+console.log('The average tip for Jhon`s family' + ' ' + calcAverage(jhon.arrTips));
+console.log('The average tip for Mark`s family' + ' ' + calcAverage(mark.arrTips));
+
+if (calcAverage(jhon.arrTips) > calcAverage(mark.arrTips)) {
+    console.log(jhon.family + 'family paid the highest tips on average');
+    } else if (calcAverage(jhon.arrTips) < calcAverage(mark.arrTips)) {
+    console.log(mark.family + ' '+ 'family paid the highest tips on average');
+    } else {
+    console.log(mark.family + jhon.family + 'families paid the same');
+}
+
+
+

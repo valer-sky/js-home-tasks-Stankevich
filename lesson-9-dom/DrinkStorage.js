3ab267c7290627169fe28eb390af6a45b38d8588
valer-sky <89993618+valer-sky@users.noreply.github.com>
2022-03-04 20:30:42 +0300

    corrected the mistake

--- lesson-9-dom/DrinkStorage.js
+++ lesson-9-dom/DrinkStorage.js
@@ -5,10 +5,12 @@ var drinkName = document.getElementById('drinkName');
 drinkName.onclick = function() {
 	var nameDr;
 	var coctails = {};
-	coctails.nameDr = prompt("напишите название напитка");
+	nameDr = prompt("напишите название напитка");
+	coctails.nameDr = nameDr;
 	coctails.alcohol = confirm(nameDr + " - алкогольный напиток или нет?\nok - алкогольный\nотмена - без алкогольный");
 	coctails.recipe = prompt("напишите рецепт напитка - " + nameDr);
 	console.log(coctails);
 	drinkStorage.addValue(nameDr, coctails);
 	console.log(drinkStorage);
+	// document.getElementById('#drinkInfoP').innerHTML = coctails;
 }

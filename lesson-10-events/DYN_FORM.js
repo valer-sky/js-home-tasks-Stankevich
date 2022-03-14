11ba7e1551905e252adfd13da150dc139f47fcb9
valer-sky <89993618+valer-sky@users.noreply.github.com>
2022-03-14 15:33:53 +0300

    edited indents

--- lesson-10-events/DYN_FORM.js
+++ lesson-10-events/DYN_FORM.js
@@ -31,92 +31,89 @@ function form_create(arr,form){
             newInputElement.name = element.name;
             newInputElement.placeholder = element.placeholder;
 
-            if(element.required){
-                newInputElement.required = true;
-            }
-                newInputElement.classList.add(element.class);
-                newLabelElement.appendChild(newTextElement);
-                newLabelElement.appendChild(newInputElement);
+        if(element.required) {
+            newInputElement.required = true;
+        }
+            newInputElement.classList.add(element.class);
+            newLabelElement.appendChild(newTextElement);
+            newLabelElement.appendChild(newInputElement);
                 newTagElement.appendChild(newLabelElement);
                 form.appendChild(newTagElement);
-            }
-            if(element.type == "submit"){
-                var newTagElement = document.createElement('p');
-                var newInputElement = document.createElement('input');
-                newInputElement.type = element.type;
-                newInputElement.value = element.value;
+        }
+        if(element.type == "submit"){
+            var newTagElement = document.createElement('p');
+            var newInputElement = document.createElement('input');
+            newInputElement.type = element.type;
+            newInputElement.value = element.value;
+            newInputElement.classList.add("btn","btn-primary");
+            newTagElement.appendChild(newInputElement);
+            form.appendChild(newTagElement);
+        }
+        if(element.type=="select"){
+            var newTagElement = document.createElement('p');
+            var newLabelElement = document.createElement('label');
+            var newTextElement = document.createTextNode(element.label);
+            var newSelectElement = document.createElement('select');
+            newSelectElement.name = element.name;
+            newSelectElement.classList.add(element.class);
+            newLabelElement.appendChild(newTextElement);
 
-                newInputElement.classList.add("btn","btn-primary");
-                newTagElement.appendChild(newInputElement);
-                form.appendChild(newTagElement);
+            for(var i = 0;i < element.options.length; i++){
+                var newOptionElement = document.createElement('option');
+                newOptionElement.value = element.options[i].value;
+                var newTextElement = document.createTextNode(element.options[i].text);
+                newOptionElement.appendChild(newTextElement);
+                newSelectElement.appendChild(newOptionElement);
             }
-            if(element.type=="select"){
+            newLabelElement.appendChild(newSelectElement);
+            newTagElement.appendChild(newLabelElement);
+            form.appendChild(newTagElement);
+            }
+        if(element.type == "radio"){
+            var newTagElement = document.createElement('p');
+            var newLabelElement = document.createElement('label');
+            var newTextElement = document.createTextNode(element.label);
+            newLabelElement.appendChild(newTextElement);
+            newTagElement.appendChild(newLabelElement);
 
-                var newTagElement = document.createElement('p');
-                var newLabelElement = document.createElement('label');
-                var newTextElement = document.createTextNode(element.label);
-                var newSelectElement = document.createElement('select');
-                newSelectElement.name = element.name;
-                newSelectElement.classList.add(element.class);
-                newLabelElement.appendChild(newTextElement);
-
-                for(var i = 0;i < element.options.length; i++){
-                    var newOptionElement = document.createElement('option');
-                    newOptionElement.value = element.options[i].value;
-                    var newTextElement = document.createTextNode(element.options[i].text);
-                    newOptionElement.appendChild(newTextElement);
-                    newSelectElement.appendChild(newOptionElement);
-                }
-                newLabelElement.appendChild(newSelectElement);
-                newTagElement.appendChild(newLabelElement);
-                form.appendChild(newTagElement);
+            for( var i = 0; i<element.options.length; i++){
+                var newRadioElement = document.createElement('input');
+                newRadioElement.value = element.options[i].value;
+                newRadioElement.type = "radio";
+                newRadioElement.name = element.name;
+                var newLabelRElement = document.createElement('label');
+                var newTextElement = document.createTextNode(element.options[i].text);
+                newLabelRElement.appendChild(newRadioElement);
+                newLabelRElement.appendChild(newTextElement);
+                newTagElement.appendChild(newLabelRElement);
             }
-            if(element.type == "radio"){
-                var newTagElement = document.createElement('p');
-                var newLabelElement = document.createElement('label');
-                var newTextElement = document.createTextNode(element.label);
-                newLabelElement.appendChild(newTextElement);
-                newTagElement.appendChild(newLabelElement);
-
-                for( var i = 0; i<element.options.length; i++){
-
-                    var newRadioElement = document.createElement('input');
-                    newRadioElement.value = element.options[i].value;
-                    newRadioElement.type = "radio";
-                    newRadioElement.name = element.name;
-                    var newLabelRElement = document.createElement('label');
-                    var newTextElement = document.createTextNode(element.options[i].text);
-                    newLabelRElement.appendChild(newRadioElement);
-                    newLabelRElement.appendChild(newTextElement);
-                    newTagElement.appendChild(newLabelRElement);
-                }
-                form.appendChild(newTagElement);
-            }
-            if(element.type == "checkbox"){
-                var newTagElement = document.createElement('p');
-                var newLabelElement = document.createElement('label');
-                var newTextElement = document.createTextNode(element.label);
-                newLabelElement.appendChild(newTextElement);
-                var newCheckElement = document.createElement('input');
-                newCheckElement.type = "checkbox";
-                newCheckElement.name = element.name;
-                (element.checked) ? newCheckElement.checked = "true" : newCheckElement.checked = "false";
-                newLabelElement.appendChild(newCheckElement);
-                newTagElement.appendChild(newLabelElement);
-                form.appendChild(newTagElement);
-            }
-            if(element.type =="textarea"){
-                var newTagElement = document.createElement('p');
-                var newLabelElement = document.createElement('label');
-                var newTextElement = document.createTextNode(element.label);
-                newLabelElement.appendChild(newTextElement);
-                var newTextareaElement = document.createElement('textarea');
-                newTextareaElement.name = element.name;
-                newTextareaElement.classList.add(element.class);
-                newLabelElement.appendChild(newTextareaElement);
-                newTagElement.appendChild(newLabelElement);
-                form.appendChild(newTagElement);
-            }
+            form.appendChild(newTagElement);
+        }
+        if(element.type == "checkbox"){
+            var newTagElement = document.createElement('p');
+            var newLabelElement = document.createElement('label');
+            var newTextElement = document.createTextNode(element.label);
+            newLabelElement.appendChild(newTextElement);
+            var newCheckElement = document.createElement('input');
+            newCheckElement.type = "checkbox";
+            newCheckElement.name = element.name;
+            (element.checked) ? newCheckElement.checked = "true" : newCheckElement.checked = "false";
+            newLabelElement.appendChild(newCheckElement);
+            newTagElement.appendChild(newLabelElement);
+            form.appendChild(newTagElement);
+        }
+        if(element.type =="textarea"){
+            var newTagElement = document.createElement('p');
+            var newLabelElement = document.createElement('label');
+            var newTextElement = document.createTextNode(element.label);
+            newLabelElement.appendChild(newTextElement);
+            var newTextareaElement = document.createElement('textarea');
+            newTextareaElement.name = element.name;
+            newTextareaElement.classList.add(element.class);
+            newLabelElement.appendChild(newTextareaElement);
+            newTagElement.appendChild(newLabelElement);
+            form.appendChild(newTagElement);
+        }
     }
 }
 form_create(Test,forms);

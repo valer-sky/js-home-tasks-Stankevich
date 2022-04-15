286843e49a4b1213cae8cc449431cbef52f8c9b9
valer-sky <89993618+valer-sky@users.noreply.github.com>
2022-04-15 16:07:20 +0300

    Created a new branch and folder in the repository

---
+++ lesson-19-mvc/LocalStorage.js
@@ -0,0 +1,23 @@
+"use strict";
+
+function THashStorage() {
+	this.hashStorage = {};
+}
+THashStorage.prototype.addValue = function(key, value) {
+	this.hashStorage[key] = value;
+};
+THashStorage.prototype.getValue = function(key) {
+	return this.hashStorage[key];
+	};
+THashStorage.prototype.deleteValue = function(key) {
+	if (!(key in this.hashStorage)) {
+		return false;
+	}
+};
+THashStorage.prototype.getKeys = function() {
+	var keys = [];
+	for (var key in this.hashStorage) {
+		keys.push(key);
+	}
+	return keys;
+};
